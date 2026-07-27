507f8131383a2e284a08b6fcf845ef2913087a83
Danzy-189 <danzypetrykin@gmail.com>
2026-07-27 23:44:45 +0300

    Refactor WINCH and TOWBAR to use BlockItem

--- src/main/java/dev/danzy/carwinch/registry/CWItems.java
+++ src/main/java/dev/danzy/carwinch/registry/CWItems.java
@@ -2,8 +2,8 @@ package dev.danzy.carwinch.registry;
 
 import dev.danzy.carwinch.CarWinch;
 import dev.danzy.carwinch.content.item.IronRopeItem;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -13,9 +13,10 @@ public class CWItems {
     public static final DeferredItem<IronRopeItem> IRON_ROPE = ITEMS.register("iron_rope",
             () -> new IronRopeItem(new Item.Properties().stacksTo(16)));
 
-    public static final DeferredItem<Item> WINCH = ITEMS.registerSimpleBlockItem("winch", CWBlocks.WINCH);
-    public static final DeferredItem<Item> TOWBAR = ITEMS.registerSimpleBlockItem("towbar", CWBlocks.TOWBAR);
-
+    public static final DeferredItem<BlockItem> WINCH =
+        ITEMS.registerSimpleBlockItem("winch", CWBlocks.WINCH);
+    public static final DeferredItem<BlockItem> TOWBAR =
+        ITEMS.registerSimpleBlockItem("towbar", CWBlocks.TOWBAR);
     private CWItems() {
     }
 }
