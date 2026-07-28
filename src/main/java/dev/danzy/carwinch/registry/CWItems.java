fc54cfea73afb7c0e12ab54bcdbb7b8454f1d725
Danzy-189 <danzypetrykin@gmail.com>
2026-07-28 20:59:43 +0300

    Update CWItems.java

--- src/main/java/dev/danzy/carwinch/registry/CWItems.java
+++ src/main/java/dev/danzy/carwinch/registry/CWItems.java
@@ -3,6 +3,7 @@ package dev.danzy.carwinch.registry;
 import dev.danzy.carwinch.CarWinch;
 import dev.danzy.carwinch.content.item.CouplingItem;
 import dev.danzy.carwinch.content.item.IronRopeItem;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -28,10 +29,10 @@ public class CWItems {
                     )
             );
 
-    public static final DeferredItem WINCH =
+    public static final DeferredItem<BlockItem> WINCH =
             ITEMS.registerSimpleBlockItem("winch", CWBlocks.WINCH);
 
-    public static final DeferredItem TOWBAR =
+    public static final DeferredItem<BlockItem> TOWBAR =
             ITEMS.registerSimpleBlockItem("towbar", CWBlocks.TOWBAR);
 
     private CWItems() {
