29bca64a2f05a2524f90bdd0b3afb0211bfdb1be
Danzy-189 <danzypetrykin@gmail.com>
2026-07-28 20:21:57 +0300

    Update CWItems.java

--- src/main/java/dev/danzy/carwinch/registry/CWItems.java
+++ src/main/java/dev/danzy/carwinch/registry/CWItems.java
@@ -1,24 +1,39 @@
 package dev.danzy.carwinch.registry;
 
 import dev.danzy.carwinch.CarWinch;
+import dev.danzy.carwinch.content.item.CouplingItem;
 import dev.danzy.carwinch.content.item.IronRopeItem;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class CWItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CarWinch.ID);
 
-    public static final DeferredItem<IronRopeItem> IRON_ROPE = ITEMS.register("iron_rope",
-            () -> new IronRopeItem(new Item.Properties().stacksTo(16)));
+    public static final DeferredRegister.Items ITEMS =
+            DeferredRegister.createItems(CarWinch.ID);
 
-    public static final DeferredItem<BlockItem> WINCH =
+    public static final DeferredItem<IronRopeItem> IRON_ROPE =
+            ITEMS.register(
+                    "iron_rope",
+                    () -> new IronRopeItem(
+                            new Item.Properties().stacksTo(16)
+                    )
+            );
+
+    public static final DeferredItem<CouplingItem> COUPLING =
+            ITEMS.register(
+                    "coupling",
+                    () -> new CouplingItem(
+                            new Item.Properties().stacksTo(16)
+                    )
+            );
+
+    public static final DeferredItem WINCH =
             ITEMS.registerSimpleBlockItem("winch", CWBlocks.WINCH);
-    public static final DeferredItem<BlockItem> TOWBAR =
+
+    public static final DeferredItem TOWBAR =
             ITEMS.registerSimpleBlockItem("towbar", CWBlocks.TOWBAR);
 
     private CWItems() {
     }
 }
-
