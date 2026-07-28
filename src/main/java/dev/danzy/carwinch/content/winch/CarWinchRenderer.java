499349e002ddb89df67aaa0d50446dac835a5dc4
Danzy-189 <danzypetrykin@gmail.com>
2026-07-28 04:26:57 +0300

    Update CarWinchRenderer.java

--- src/main/java/dev/danzy/carwinch/content/winch/CarWinchRenderer.java
+++ src/main/java/dev/danzy/carwinch/content/winch/CarWinchRenderer.java
@@ -2,30 +2,49 @@ package dev.danzy.carwinch.content.winch;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.foundation.blockEntity.renderer.SafeBlockEntityRenderer;
-import dev.simulated_team.simulated.content.blocks.rope.strand.client.RopeStrandRenderer;
+import dev.danzy.carwinch.client.CarWinchRopeRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.phys.Vec3;
 
-/** Draws the physics rope strand owned by the winch. */
-public class CarWinchRenderer extends SafeBlockEntityRenderer<CarWinchBlockEntity> {
+public class CarWinchRenderer
+        extends SafeBlockEntityRenderer {
 
-    public CarWinchRenderer(final BlockEntityRendererProvider.Context context) {
+    public CarWinchRenderer(
+            final BlockEntityRendererProvider.Context context
+    ) {
     }
 
     @Override
-    public boolean shouldRenderOffScreen(final CarWinchBlockEntity be) {
+    public boolean shouldRenderOffScreen(
+            final CarWinchBlockEntity blockEntity
+    ) {
         return true;
     }
 
     @Override
-    public boolean shouldRender(final CarWinchBlockEntity be, final Vec3 cameraPos) {
+    public boolean shouldRender(
+            final CarWinchBlockEntity blockEntity,
+            final Vec3 cameraPosition
+    ) {
         return true;
     }
 
     @Override
-    protected void renderSafe(final CarWinchBlockEntity be, final float partialTicks, final PoseStack ms,
-                              final MultiBufferSource buffer, final int light, final int overlay) {
-        RopeStrandRenderer.render(be, be.getRopeHolder(), partialTicks, ms, buffer);
+    protected void renderSafe(
+            final CarWinchBlockEntity blockEntity,
+            final float partialTicks,
+            final PoseStack poseStack,
+            final MultiBufferSource buffer,
+            final int packedLight,
+            final int packedOverlay
+    ) {
+        CarWinchRopeRenderer.render(
+                blockEntity,
+                blockEntity.getRopeHolder(),
+                partialTicks,
+                poseStack,
+                buffer
+        );
     }
 }
