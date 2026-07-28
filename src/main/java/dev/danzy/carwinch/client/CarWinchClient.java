e79da0d8aea4cbc3dcd0d8dbc64c1d857ed3e817
Danzy-189 <danzypetrykin@gmail.com>
2026-07-28 04:28:46 +0300

    Update CarWinchClient.java

--- src/main/java/dev/danzy/carwinch/client/CarWinchClient.java
+++ src/main/java/dev/danzy/carwinch/client/CarWinchClient.java
@@ -1,5 +1,5 @@
 package dev.danzy.carwinch.client;
-import dev.danzy.carwinch.client.CarWinchPartialModels;
+
 import dev.danzy.carwinch.CarWinch;
 import dev.danzy.carwinch.content.towbar.TowbarRenderer;
 import dev.danzy.carwinch.content.winch.CarWinchRenderer;
@@ -9,30 +9,28 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
-@EventBusSubscriber(modid = CarWinch.ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class CarWinchClient {
-    
-    @SubscribeEvent
-public static void onRegisterRenderers(
-        final EntityRenderersEvent.RegisterRenderers event
-) {
-    CarWinchPartialModels.init();
-
-    event.registerBlockEntityRenderer(
-            CWBlockEntities.WINCH.get(),
-            CarWinchRenderer::new
-    );
-
-    event.registerBlockEntityRenderer(
-            CWBlockEntities.TOWBAR.get(),
-            TowbarRenderer::new
-    );
-}
+@EventBusSubscriber(
+        modid = CarWinch.ID,
+        bus = EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+)
+public final class CarWinchClient {
 
     @SubscribeEvent
-    public static void onRegisterRenderers(final EntityRenderersEvent.RegisterRenderers event) {
-        event.registerBlockEntityRenderer(CWBlockEntities.WINCH.get(), CarWinchRenderer::new);
-        event.registerBlockEntityRenderer(CWBlockEntities.TOWBAR.get(), TowbarRenderer::new);
+    public static void onRegisterRenderers(
+            final EntityRenderersEvent.RegisterRenderers event
+    ) {
+        CarWinchPartialModels.init();
+
+        event.registerBlockEntityRenderer(
+                CWBlockEntities.WINCH.get(),
+                CarWinchRenderer::new
+        );
+
+        event.registerBlockEntityRenderer(
+                CWBlockEntities.TOWBAR.get(),
+                TowbarRenderer::new
+        );
     }
 
     private CarWinchClient() {
