00647aca99376a08a1c1a996ae89bcde282e5942
Danzy-189 <danzypetrykin@gmail.com>
2026-07-28 20:22:10 +0300

    Update CWTabs.java

--- src/main/java/dev/danzy/carwinch/registry/CWTabs.java
+++ src/main/java/dev/danzy/carwinch/registry/CWTabs.java
@@ -9,19 +9,27 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class CWTabs {
-    public static final DeferredRegister<CreativeModeTab> TABS =
-            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CarWinch.ID);
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN = TABS.register("main",
-            () -> CreativeModeTab.builder()
-                    .title(Component.translatable("itemGroup.carwinch"))
-                    .icon(() -> new ItemStack(CWItems.WINCH.get()))
-                    .displayItems((params, output) -> {
-                        output.accept(CWItems.WINCH.get());
-                        output.accept(CWItems.TOWBAR.get());
-                        output.accept(CWItems.IRON_ROPE.get());
-                    })
-                    .build());
+    public static final DeferredRegister<CreativeModeTab> TABS =
+            DeferredRegister.create(
+                    Registries.CREATIVE_MODE_TAB,
+                    CarWinch.ID
+            );
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN =
+            TABS.register(
+                    "main",
+                    () -> CreativeModeTab.builder()
+                            .title(Component.translatable("itemGroup.carwinch"))
+                            .icon(() -> new ItemStack(CWItems.WINCH.get()))
+                            .displayItems((params, output) -> {
+                                output.accept(CWItems.WINCH.get());
+                                output.accept(CWItems.TOWBAR.get());
+                                output.accept(CWItems.IRON_ROPE.get());
+                                output.accept(CWItems.COUPLING.get());
+                            })
+                            .build()
+            );
 
     private CWTabs() {
     }
